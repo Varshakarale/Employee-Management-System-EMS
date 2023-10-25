ef59dc48828787af4f3d5acaaa159fe8521cf215
Varsha Karale <110241261+Varshakarale@users.noreply.github.com>
2023-10-25 19:27:26 +0530

    Delete dao directory

--- dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectems.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.projectems.entities.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
