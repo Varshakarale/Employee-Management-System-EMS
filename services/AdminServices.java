8ecba4e0d45a4f05295d5688a1d2b7294bc9d14b
Varsha Karale <110241261+Varshakarale@users.noreply.github.com>
2023-10-28 21:13:26 +0530

    Add files via upload

---
+++ services/AdminServices.java
@@ -0,0 +1,34 @@
+package com.projectems.services;
+
+
+import java.util.List;
+
+import com.projectems.dto.AdminDTO;
+
+public class AdminServices {
+
+    public AdminDTO createAdmin(AdminDTO adminDTO) {
+        
+        return null; 
+    }
+
+    public AdminDTO getAdminById(Long id) {
+     
+        return null; 
+    }
+
+    public AdminDTO updateAdmin(Long id, AdminDTO adminDTO) {
+       
+        return null; 
+    }
+
+    public String deleteAdmin(Long id) {
+      
+        return "Admin with ID " + id + " has been deleted successfully.";
+    }
+
+    public List<AdminDTO> getAllAdmins() {
+       
+        return null; 
+    }
+}
